073c8217a84f9a2baac9e9783581940e90a3d3e1
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2016-09-10 18:27:51 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM ubuntu:latest
+MAINTAINER Jan Blaha
+EXPOSE 5000
+
+RUN apt-get update && apt-get install -y sudo
+RUN apt-get install -y  curl
+RUN curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+RUN apt-get install -y nodejs docker.io build-essential
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+COPY package.json /usr/src/app/
+RUN npm install
+
+RUN mkdir /run-data
+
+COPY . /usr/src/app
+COPY patch /usr/src/app
+
+EXPOSE 5000
+CMD [ "node", "index.js" ]
---
+++ index.js
@@ -0,0 +1,66 @@
+const http = require('http')
+const execFile = require('child_process').execFile
+const fs = require('fs')
+const wkhtmltopdf = require('wkhtmltopdf-installer')
+const uuid = require('uuid').v1
+const async = require('async')
+
+const processPart = (opts, id, partName, cb) => {
+  if (!opts[partName]) {
+    return cb()
+  }
+
+  fs.writeFile(`${id}-${partName}.html`, opts[partName], (err) => {
+    if (err) {
+      return cb(err)
+    }
+
+    opts.args.push(`--${partName}`)
+    opts.args.push(`${id}-${partName}.html`)
+    cb()
+  })
+}
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    return res.end('OK')
+  }
+  var data = ''
+  req.on('data', function (chunk) {
+    data += chunk.toString()
+  })
+
+  req.on('end', function () {
+    console.log(data)
+    const opts = JSON.parse(data)
+
+    const id = uuid()
+
+    async.waterfall([
+      (cb) => fs.writeFile(`${id}.html`, opts.html, cb),
+      (cb) => processPart(opts, id, 'header-html', cb),
+      (cb) => processPart(opts, id, 'footer-html', cb),
+      (cb) => processPart(opts, id, 'cover', cb),
+      (cb) => {
+        opts.args.push(`${id}.html`)
+        opts.args.push(`${id}.pdf`)
+        console.log(opts.args)
+        cb()
+      },
+      (cb) => execFile(wkhtmltopdf.path, opts.args, cb)], (err) => {
+      console.log('done')
+      if (err) {
+        res.statusCode = 500
+        res.setHeader('Content-Type', 'text/plain')
+        return res.end('Error when executing wkhtmltopdf ' + err.stack)
+      }
+
+      const stream = fs.createReadStream(`${id}.pdf`)
+      stream.pipe(res)
+    })
+  })
+})
+
+server.listen(5000)
